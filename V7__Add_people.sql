9e931447d6410e66987919c9470adecec95d2b07
shivam3108 <38430556+shivam3108@users.noreply.github.com>
2019-04-17 15:25:46 +0530

    Update V7__Add_people.sql

--- V7__Add_people.sql
+++ V7__Add_people.sql
@@ -1,3 +1,3 @@
-insert into PERSON (ID, NAME) values (115, 'Axel2212');
-insert into PERSON (ID, NAME) values (126, 'Mr. Foo2212');
-insert into PERSON (ID, NAME) values (317, 'Ms. Bar221');
+insert into PERSON (ID, NAME) values (1, 'Axel');
+insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
